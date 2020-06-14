f3d8745ffa87a4395d664aed6a059f341c775ae0
Samantha <samantha@phygitalker.com>
2020-06-14 12:01:53 +0800

    [WEEK2] homework1 2 3 completed

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,10 @@
 function printStars(n) {
-  
+    if( 1<=n && n<=30 ) {
+        for (i=0; i<n; i ++) {
+            console.log("*", i)
+        }        
+    }
+
 }
 
 printStars(5);
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,6 @@
 function capitalize(str) {
-  
+  string = str[0].toUpperCase() + str.slice(1);
+  return string;
 }
 
 console.log(capitalize('hello'));
